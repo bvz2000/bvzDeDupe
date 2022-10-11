449aa8f562a7f39ac815c0cf472aa320017e3123
bvz <bvz@c381.com>
2022-10-10 22:08:02 -0700

    Switched to storing index instead of object itself.
    Also fixed bug where we were storing the index as a list

--- src/collection.py
+++ src/collection.py
@@ -12,22 +12,22 @@ class Collection(object):
     def __post_init__(self):
         self.items = dict()
 
-    def add(self,
-            key,
-            item):
+    def store_index(self,
+                    key,
+                    index):
         """
         Adds an item to the dictionary.
 
-        :param key: The key for the item to be added.
-        :param item: The AFile object to add to this key.
+        :param key: The key for the item to be added (something like size, name, parent_name, or type).
+        :param index: The index of the AFile object in the master afile_objs list.
 
         :return: Nothing.
         """
 
         try:
-            self.items[key].append(item)
+            self.items[key].indices.append(index)
         except KeyError:
             by_attribute_obj = ByAttribute(key)
-            by_attribute_obj.objs.append(item)
-            self.items[key] = [by_attribute_obj]
+            by_attribute_obj.indices.append(index)
+            self.items[key] = by_attribute_obj
 
