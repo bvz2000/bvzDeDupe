96e70a369eca8d16c9c5594bb63f3e8febfe117e
bvz <bvz@c381.com>
2022-10-27 00:35:14 -0700

    Fixed issue where the canonical scan was not respecting the user settings

--- src/session.py
+++ src/session.py
@@ -90,7 +90,14 @@ class Session(object):
         """
 
         self.canonical_scan = CanonicalDir(scan_dir=self.canonical_dir)
-        for file_count in self.canonical_scan.scan(report_frequency=self.report_frequency):
+        for file_count in self.canonical_scan.scan(skip_sub_dir=self.skip_sub_dir,
+                                                   skip_hidden=self.skip_hidden,
+                                                   skip_zero_len=self.skip_zero_len,
+                                                   incl_dir_regexes=self.incl_dir_regex,
+                                                   excl_dir_regexes=self.excl_dir_regex,
+                                                   incl_file_regexes=self.incl_file_regex,
+                                                   excl_file_regexes=self.excl_file_regex,
+                                                   report_frequency=self.report_frequency):
             yield file_count
 
     # ------------------------------------------------------------------------------------------------------------------
