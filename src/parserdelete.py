49b506c6668899ffbca9911aef4b80ba7559fd55
bvz <bvz@c381.com>
2023-03-18 14:36:03 -0700

    Changing commmand line args

--- src/parserdelete.py
+++ src/parserdelete.py
@@ -6,17 +6,15 @@ from argparse import ArgumentParser
 import os.path
 
 help_msg = f"""
-A program to delete files that were identified as duplicates by the bin app. This app requires
-that the bin app be run first using the -o (output file) option. That output file which lists
-the results of the comparison operation is used to determine which files to delete.
+CA program to delete files that were identified as duplicates by the compareFolders app. The deleteFiles app requires
+that the compareFolders app be run first using the -o (output file) option. That output file (which lists
+the results of the comparison operation) is used to determine which files to delete.
 
-By default, the delete files operation runs a sanity check before deleting each file. This can be as simple as
-comparing the file size to make sure the files are still likely to be identical before deleting the duplicate file
-in the query directory. But it can also run a checksum to make sure that the two files are actually bit for bit
-identical before deleting. This will, of course, add a fair bit of time to the delete operation. Additional checks,
-like whether the names match, the parent directory matches, the relative path matches, etc. can also be applied as
-part of this sanity check. But these can only be applied if they had been used in the original bin
-operation.
+By default, the delete files operation runs a sanity check before deleting each file. By default these sanity checks
+include checking the file sizes to make sure they are the same, and running a full checksum on both files. Running the
+checksum will, of course, add a fair bit of time to the delete operation. The checksum may be skipped if desired. 
+Additional checks, like whether the names match, the parent directory matches, the relative path matches, etc. are also 
+be applied as part of this sanity check if they had been used in the original compareFolders operation.
 
 Additional options include simply renaming the files with the prefix: "compareFoldersPendingDelete_" instead of actually
 deleting the files.
@@ -41,43 +39,39 @@ class Parser(object):
 
         self.parser = ArgumentParser(description=help_msg)
 
-        help_str = "The log file written out by the bin command."
+        help_str = "The log file written out by the compareFolders command."
         self.parser.add_argument('log_file',
                                  metavar='log_file',
                                  type=str,
                                  help=help_str)
 
-        help_str = "Perform a check to ensure that the MD5 checksum of each file matches before deleting or " \
-                   "renaming. By default each file is checked using the same comparison operations performed by the " \
-                   "original bin session. Size is always compared. Name, Parent Directory Name, Relative " \
-                   "Path, File Type, Creation Time, and Modification Time will also be compared if any of these were " \
-                   "used in the original bin session. But a checksum is NOT automatically run. Use this " \
-                   "option to force the checksum. Note: this will slow down the delete or rename operation by a " \
-                   "significant amount."
-        self.parser.add_argument("-c",
-                                 dest="checksum",
+        help_str = "Skip checksum. By default an MD5 checksum of each file is done before deleting or renaming. " \
+                   "Since the checksum will slow down the delete or rename operation by a significant amount, this " \
+                   "option will skip this checksum."
+        self.parser.add_argument("-S",
+                                 dest="skip_checksum",
                                  action="store_true",
                                  help=help_str)
 
         help_str = "Rename files instead of deleting them. A prefix of \"compareFoldersPendingDelete_\" will be " \
                    "added to the beginning of each file that would otherwise be deleted."
-        self.parser.add_argument("-r",
+        self.parser.add_argument("-R",
                                  dest="rename",
                                  action="store_true",
                                  help=help_str)
 
         help_str = "Trial run. This will print out any actions that would have been taken (deleting or renaming) " \
-                   "instead of actually renaming or deleting any files. Use the -s option to force the trial to run " \
+                   "instead of actually renaming or deleting any files. Use the -q option to force the trial to run " \
                    "without printing out any commands."
-        self.parser.add_argument("-t",
+        self.parser.add_argument("-T",
                                  dest="trial",
                                  action="store_true",
                                  help=help_str)
 
-        help_str = "Silent trial. This will prevent the trial run from displaying any information. The trial is " \
+        help_str = "Quiet trial. This will prevent the trial run from displaying any information. The trial is " \
                    "still performed, but the outputs mimic that of a real run."
-        self.parser.add_argument("-s",
-                                 dest="silent_trial",
+        self.parser.add_argument("-q",
+                                 dest="quiet_trial",
                                  action="store_true",
                                  help=help_str)
 
